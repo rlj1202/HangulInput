2af93ab621d5e91925b76b7e749f2118acfd60bd
Jisu Sim <rlj1202@gmail.com>
2019-08-29 17:19:54 +0900

    First commit

---
+++ src/main/java/redlaboratory/hangulinput/HangulInput.java
@@ -0,0 +1,147 @@
+package redlaboratory.hangulinput;
+
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.screen.EditSignScreen;
+import net.minecraft.client.gui.screen.WorldSelectionScreen;
+import net.minecraft.client.gui.screen.inventory.AnvilScreen;
+import net.minecraft.client.gui.screen.inventory.CreativeScreen;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.*;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
+import redlaboratory.hangulutils.HangulBuilder;
+
+@Mod(HangulInput.MODID)
+public class HangulInput {
+
+    public static final String MODID = "hangulinput";
+
+    private static final HangulBuilder HANGUL_BUILDER = new HangulBuilder();
+
+    private static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    public HangulInput() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public void onGuiInitPost(GuiScreenEvent.InitGuiEvent.Post event) {
+        if (event.getGui() == null) return;
+
+        int guiwidth = event.getGui().width;
+        int guiheight = event.getGui().height;
+
+        if (event.getGui() instanceof ChatScreen) {
+            event.addWidget(new InputModeIndicatorWidget(
+                    2,
+                    guiheight - 2 - 12*2 - 2,
+                    12, 12, "", HANGUL_BUILDER
+            ));
+        } else if (event.getGui() instanceof EditSignScreen) {
+            event.addWidget(new InputModeIndicatorWidget(
+                    guiwidth / 2 - 12 / 2,
+                    52,
+                    12, 12, "", HANGUL_BUILDER
+            ));
+        } else if (event.getGui() instanceof AnvilScreen) {
+            event.addWidget(new InputModeIndicatorWidget(
+                    guiwidth / 2 - 83,
+                    guiheight / 2 - 78,
+                    12, 12, "", HANGUL_BUILDER
+            ));
+        } else if (event.getGui() instanceof CreativeScreen) {
+            event.addWidget(new InputModeIndicatorWidget(
+                    guiwidth / 2 + 77,
+                    guiheight / 2 - 64,
+                    12, 12, "", HANGUL_BUILDER
+            ));
+        } else if (event.getGui() instanceof WorldSelectionScreen) {
+            event.addWidget(new InputModeIndicatorWidget(
+                    guiwidth / 2 - 125,
+                    25,
+                    12, 12, "", HANGUL_BUILDER
+            ));
+        }
+    }
+
+    @SubscribeEvent()
+    @OnlyIn(Dist.CLIENT)
+    public void onGuiOpen(GuiOpenEvent event) {
+        HANGUL_BUILDER.clearStates();
+
+        LOGGER.info("clear states");
+    }
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public void onGuiKeyboardPressed(GuiScreenEvent.MouseClickedEvent event) {
+        HANGUL_BUILDER.clearStates();
+
+        LOGGER.info("clear states");
+    }
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public void onGuiKeyboardPressed(GuiScreenEvent.KeyboardKeyPressedEvent.Pre event) {
+        if (event.getKeyCode() == GLFW.GLFW_KEY_LEFT_CONTROL) {
+            HANGUL_BUILDER.toggleHangulMode();
+            HANGUL_BUILDER.clearStates();
+
+            LOGGER.info("hangul input mode : " + HANGUL_BUILDER.isHangulMode());
+        } else if (event.getKeyCode() == GLFW.GLFW_KEY_BACKSPACE) {
+            event.setCanceled(true);
+
+            event.getGui().keyPressed(GLFW.GLFW_KEY_BACKSPACE, event.getModifiers(), 0);
+
+            HangulBuilder.Result result = HANGUL_BUILDER.remove();
+
+            if (result.flagUpdate) {
+                event.getGui().charTyped(result.updatedLetter, 0);
+            }
+
+            if (!result.flagUpdate && !result.flagAdd) {
+                HANGUL_BUILDER.clearStates();
+            }
+
+            LOGGER.info("remove stroke : " + result);
+        } else if (event.getKeyCode() == GLFW.GLFW_KEY_UP
+                || event.getKeyCode() == GLFW.GLFW_KEY_DOWN
+                || event.getKeyCode() == GLFW.GLFW_KEY_LEFT
+                || event.getKeyCode() == GLFW.GLFW_KEY_RIGHT
+                || event.getKeyCode() == GLFW.GLFW_KEY_ESCAPE
+                || event.getKeyCode() == GLFW.GLFW_KEY_ENTER
+                || event.getKeyCode() == GLFW.GLFW_KEY_KP_ENTER) {
+            HANGUL_BUILDER.clearStates();
+
+            LOGGER.info("clear states");
+        }
+    }
+
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public void onGuiKeyboard(GuiScreenEvent.KeyboardCharTypedEvent event) {
+        if (event.getGui() == null) return;
+
+        event.setCanceled(true);
+
+        HangulBuilder.Result result = HANGUL_BUILDER.add(event.getCodePoint());
+
+        if (result.flagUpdate) {
+            event.getGui().keyPressed(GLFW.GLFW_KEY_BACKSPACE, event.getModifiers(), 0);
+            event.getGui().charTyped(result.updatedLetter, 0);
+        }
+        if (result.flagAdd) {
+            event.getGui().charTyped(result.addedLetter, 0);
+        }
+
+        LOGGER.info("key typed : " + result);
+    }
+
+}
---
+++ src/main/java/redlaboratory/hangulinput/InputModeIndicatorWidget.java
@@ -0,0 +1,39 @@
+package redlaboratory.hangulinput;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.widget.Widget;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import redlaboratory.hangulutils.HangulBuilder;
+
+@OnlyIn(Dist.CLIENT)
+public class InputModeIndicatorWidget extends Widget {
+
+    private HangulBuilder hangulBuilder;
+
+    private int bgColor = 0x80000000;
+    private int fontColor = 0xffffffff;
+
+    public InputModeIndicatorWidget(int x, int y, int width, int height, String msg, HangulBuilder hangulBuilder) {
+        super(x, y, width, height, msg);
+        this.hangulBuilder = hangulBuilder;
+    }
+
+    @Override
+    public void render(int mouseX, int mouseY, float partialTicks) {
+        Minecraft minecraft = Minecraft.getInstance();
+
+        fill(x, y, x + width, y + height, bgColor);
+        minecraft.fontRenderer.drawStringWithShadow(
+                hangulBuilder.isHangulMode() ? "\uD55C" : "\uC601",
+                x + (int) ((width - 8) / 2.0), y + (int) ((height - 8) / 2.0),
+                fontColor
+        );
+    }
+
+    @Override
+    public void renderButton(int mouseX, int mouseY, float partialTicks) {
+        super.renderButton(mouseX, mouseY, partialTicks);
+    }
+
+}
